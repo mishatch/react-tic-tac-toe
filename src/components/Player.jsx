6031e848fcd13c462662678eddb312410bd5f226
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2024-12-02 16:03:21 +0400

    finish tic tac toe game on react

---
+++ src/components/Player.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEditing = () => setIsEditing((editing) => !editing);
+  const handleChange = (event) => setPlayerName(event.target.value);
+  return (
+    <li className={isActive ? "active" : ""}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={toggleEditing}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
